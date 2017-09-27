7c9dd89196f217c5444c0a4c0aecaf213be6204a
luisjosesanchezcarrasco <luissancar@yahoo.es>
2017-09-27 20:17:40 +0200

    dos

--- app/src/main/java/com/example/luisjosesanchezcarrasco/ejemplo_calcu/MainActivity.kt
+++ app/src/main/java/com/example/luisjosesanchezcarrasco/ejemplo_calcu/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.example.luisjosesanchezcarrasco.ejemplo_calcu.R.id.display
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +12,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nombreAplicacion = resources.getString(R.string.app_name)
+        display.text=nombreAplicacion.toString()
     }
 
     fun numero(v : View)
     {
         val numer=findViewById<Button>(v.id)
         val textnum=numer.text.toString()
-        display.text="${display.text.toString()} ${textnum}"
+       // display.text="${display.text.toString()} ${textnum}"
+
+
+
+
+    }
+    fun doublePa(v : View)  {
+        var b:Double=0.0
+
+
+        var a=display.text.toString().toDouble()
+        if (a!=b) {
+            a=b
+
+    }
 
     }
 }
